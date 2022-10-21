97b80fd07411e1261156c3c7fab20f2cc63fcf4b
LucasGad <gad.alla.lucas@iisgalvanimi.edu.it>
2022-10-21 09:40:51 +0000

    consegna

---
+++ main1.py
@@ -0,0 +1,37 @@
+#stampa x che è 5 e stampa y che è john
+x="5"
+y="john"
+print(x)
+print(y)
+
+#si usa str per nominare che il 3 è una stringa, invece float viene usato per numeri con la virgola 
+x = str(3)    # x will be '3'
+y = int(3)    # y will be 3
+z = float(3)  # z will be 3.0
+
+#in questo caso (type(x)) viene usato per capire di che variabile è 
+x = 5
+y = "John"
+print(type(x))
+print(type(y))
+
+#le due virgolette sono uguali a la sola virgoletta 
+x = "John"
+# is the same as
+x = 'John'
+
+#non è possibile assegnare due elementi ala stessa variabile ma bisogna creare due variabili 
+a = 4
+A = "Sally"
+#A will not overwrite a
+
+
+
+myvar = "John"
+my_var = "John"
+_my_var = "John"
+myVar = "John"
+MYVAR = "John"
+myvar2 = "John"
+
+
